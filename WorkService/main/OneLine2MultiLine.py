df7237dbcf451f699eda786ca8ef9d9a6cbd7d52
yuxiang.hyx <yuxiang.hyx@MININT-MHD3UCS.hz.ali.com>
2015-04-13 14:43:31 +0800

    dfdf

--- WorkService/main/OneLine2MultiLine.py
+++ WorkService/main/OneLine2MultiLine.py
@@ -1,6 +1,7 @@
+#coding=utf-8 
 from numpy import *
 fr = open('../data/oneline2multiline')
-splitchar=','
+splitchar=','          #可更改
 for line in fr.readlines():
     lines=line.strip().split(splitchar)
     for item in lines:
